05bf38e96ffc41d408f4c4aff73c4dbab584c3cc
Joshua Malone <jcharlesmalone@gmail.com>
2021-02-08 10:50:59 -0800

    "4" ~

--- 02-Homework/Develop/routes/api.js
+++ 02-Homework/Develop/routes/api.js
@@ -1,1 +1,33 @@
+const fs = require("fs")
+const path = require("path")
 
+
+function getNotes(){
+    const data = fs.readFileSync('./db/db.json', 'utf-8');
+
+}
+module.exports = function (app) {
+    app.get("/api/notes", function(req, res){
+        res.json(getNotes());
+    })
+    app.post("api/notes", (req,res) {
+        req.body.id = path();
+        const notes = getNotes();
+        notes.push(req.body)
+        
+        fs.writeFile("./db/db.json", JSON.stringify(notes), "utf-8" function(err){
+            if (err) throw err;
+            res.JSON(req.body);
+        })
+    })
+    app.delete("/api/notes/:id", function(req, res){
+        var id = req.params.id;
+        console.log(id);
+        const newNotes = getNotes().filter(notes=> note.id !== req.params.id);
+
+        fs.writeFile("./db/db.json", JSON.stringify(newNotes), "utf-8", function(err){
+            if (err) throw err;
+            res.json({sucess: true})
+        })
+    })
+};
--- 02-Homework/Develop/routes/server.js
+++ 02-Homework/Develop/routes/server.js
@@ -3,7 +3,7 @@ var express = require("express");
 var app = express();
 
 
-var PORT = process.env.PORT || 5050;
+var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
@@ -11,7 +11,7 @@ app.use(express.static("public"));
 
 
 require("./api")(app);
-// require("./html")(app);
+require("./html")(app);
 
 app.listen(PORT, function(){
     console.log("App listening on PORT " + PORT);

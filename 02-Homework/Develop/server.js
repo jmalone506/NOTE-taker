8b01970be9ef6f7f883434225df2d2dba07fe902
Joshua Malone <jcharlesmalone@gmail.com>
2021-02-08 14:19:20 -0800

    "serverchange" ~

--- 02-Homework/Develop/server.js
+++ 02-Homework/Develop/server.js
@@ -3,7 +3,7 @@ var express = require("express");
 var app = express();
 
 
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 5050;
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
